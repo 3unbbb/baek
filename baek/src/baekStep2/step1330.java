4b83aaa1170a99bb639e8cdf35e1b21d73277aa8
3unbbb <3unbbb@gmail.com>
2022-06-10 22:06:46 +0900

    if ~ing

---
+++ baek/src/baekStep2/step1330.java
@@ -0,0 +1,25 @@
+package baekStep2;
+
+import java.util.Scanner;
+
+public class step1330 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if(a>b){
+			System.out.println(">");
+		}else if(a<b){
+			System.out.println("<");
+		}else if(a==b){
+			System.out.println("==");
+		}
+		
+		
+	}
+
+}
---
+++ baek/src/baekStep2/step2753.java
@@ -0,0 +1,12 @@
+package baekStep2;
+
+public class step2753 {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
+
