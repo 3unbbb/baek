76c254d0f38e5e18f26028e788c7e35b8e548cb3
3unbbb <3unbbb@gmail.com>
2022-06-21 21:26:59 +0900

    10871

--- baek/src/baekStep3/step10871.java
+++ baek/src/baekStep3/step10871.java
@@ -12,21 +12,30 @@ public class step10871 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-			st = new StringTokenizer(br.readLine()," ");
+		StringTokenizer st
+			 = new StringTokenizer(br.readLine()," ");
 		
 		
 		int n = Integer.parseInt(st.nextToken());
 		int x = Integer.parseInt(st.nextToken());
 		
-			st = new StringTokenizer(br.readLine()," ");
+		StringBuilder sb = new StringBuilder();
+		
+		st = new StringTokenizer(br.readLine()," ");
+		
+		
 		for(int i=0; i<n; i++){
-			int a = Integer.parseInt(st.nextToken());
+			
+			int value = Integer.parseInt(st.nextToken());
+			
+			if(value<x){
+				sb.append(value).append(' ');
+			}
 			
 		}
 		
-			
+		System.out.println(sb);
+		br.close();
 
 		
 		
