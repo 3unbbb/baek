c93f61ace6051b8c066e56ec29df43fdd2d7599c
3unbbb <3unbbb@gmail.com>
2022-06-26 22:42:11 +0900

    10818

---
+++ baek/src/baekStep4/step10818.java
@@ -0,0 +1,39 @@
+package baekStep4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class step10818 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		
+		int n = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine()+" ");
+		
+		int[] arr = new int[n];
+		
+		
+		
+		for(int i = 0; i <arr.length; i++){
+			
+			arr[i] = Integer.parseInt(st.nextToken());
+			
+			
+		}
+		Arrays.sort(arr);
+		System.out.println(arr[0]+" "+arr[n-1]);
+		br.close();
+		
+	}
+	
+	
+
+}
