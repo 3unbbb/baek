c153f37d743da9414f00b394287edd5d65a7e2c8
3unbbb <3unbbb@gmail.com>
2022-11-03 18:11:04 +0900

    색종이

--- baek/src/step4/problem2563.java
+++ baek/src/step4/problem2563.java
@@ -11,46 +11,49 @@ public class problem2563 {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		
+		
 		int n = Integer.parseInt(br.readLine());
-		
-		int [][] arr = new int[n][2];
-		
+		int [][] section = new int[n][2];
+		int [][] arr = new int[100][100];
 		
 		for(int i=0;i<n;i++){
 			
 			st = new StringTokenizer(br.readLine()+" ");
 			
 			for(int j=0;j<2;j++){
-				arr[i][j] = Integer.parseInt(st.nextToken());
+				section[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		int totalSize = n*10*10;
-		
-//		arr[0][0]-arr[1][0]		arr[0][1]-arr[1][1]
-//		arr[1][0]-arr[2][0]		arr[1][1]-arr[2][1]
-//		arr[2][0]-arr[0][0]		arr[2][1]-arr[0][1]
-				
 		for(int i=0;i<n;i++){
-			int j = i+1;
+			int x = section[i][0]-1;
+			int y = section[i][1]-1;
+			int x1 = section[i][0]+9;
+			int y1 = section[i][1]+9;
 			
-			if(i==n-1){
-				j=0;
-			}
-			
-			int x = 10-Math.abs(arr[i][0]-arr[j][0]);
-			int y = 10-Math.abs(arr[i][1]-arr[j][1]);
-			
-			if(x>0 && y>0){
-			int section = x*y;
-			System.out.println("section : "+section);
-			
-			totalSize -= section;
-			System.out.println("totalSize : "+totalSize);
+			for(int t=x;t<x1;t++){
+				
+				y=section[i][1]-1;
+				for(int j=y;j<y1;j++){
+					arr[x][y] = 1;
+					y++;
+					
+					}
+				x++;
+				}
+		}
+		int cnt = 0;
+		for(int i=0;i<100;i++){
+			for(int j=0;j<100;j++){
+				if(arr[i][j] == 1){
+					cnt++;
+				}
 			}
 		}
 		
-		System.out.println(totalSize);
+		System.out.println(cnt);
+
 	}
 
 }
