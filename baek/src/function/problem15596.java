3ac4db70d1b50264e62a538b34074d6eec96c56b
3unbbb <3unbbb@gmail.com>
2022-11-04 16:04:06 +0900

    function

---
+++ baek/src/function/problem15596.java
@@ -0,0 +1,15 @@
+package function;
+
+public class problem15596 {
+
+		long sum(int[] a){
+			long ans = 0;
+			
+			for(int i=0;i<a.length; i++){
+				ans += a[i];
+			}
+			
+			return ans;
+		}
+		
+}
